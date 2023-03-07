61b1013759c159fe1ef08d928ff49359425d598f
antphb <thanhnd2462245@gmail.com>
2023-03-07 16:42:46 +0700

    sdasdsa

--- CV/settings.py
+++ CV/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'app/staticfiles'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
